d61ab004bb214caa7519974e69ee43016edd5062
RychuKropkaExe <81522252+RychuKropkaExe@users.noreply.github.com>
2021-11-26 22:54:13 +0100

    Update Zad2.sh

--- Zad2.sh
+++ Zad2.sh
@@ -1,24 +1,32 @@
-#!/user/bin/bash
-
+#!/usr/bin/bash
 ls /proc | grep '[0-9]' > /tmp/Aprc
-echo PID: USER: %CPU %MEM VSZ RSS TTY START COMMAND FILES
+echo PID: USER: %CPU %MEM VSZ STAT FILES
 while read -a line; do
         for word in ${line[@]}; do
-                echo -n "$word "
                 if test -d "/proc/$word"; then
-                userid=$(grep Uid /proc/$word/status | awk '{print $2}')
-                user=$(grep -w "$userid" /etc/passwd | cut -d: -f1)
-                echo -n  "$user "
-                fi
-                if test -d "/proc/$word"; then
+                        echo -n "$word "
+                        userid=$(grep Uid /proc/$word/status | awk '{print $2}')
+                        user=$(grep -w "$userid" /etc/passwd | cut -d: -f1)
+                        echo -n  "$user "
                         utime=$(cat /proc/$word/stat | awk '{print $14}')
                         stime=$(cat /proc/$word/stat | awk '{print $15}')
-                        uptime=$(cat /proc/uptime)
                         starttime=$(cat /proc/$word/stat | awk '{print $21}')
+                        uptime=$(cat /proc/uptime | awk '{print $1}')
                         time=$(bc -l <<< "scale=0;$utime+$stime")
                         seconds=$(bc -l <<< "scale=0;($uptime-($starttime/100))")
-                        cpus_usage=$(bc -l <<< "scale=2;100*($time/100)/$seconds")
-                        echo "$cpu_usage% "
+                        cpu_usage=$(bc -l <<< "scale=2;$time/$seconds")
+                        echo -n "$cpu_usage% "
+                        mem=$(cat /proc/$word/statm | awk '{print $2}')
+                        memU=$(bc -l <<< "scale=2; (100*($mem/1024))/7829")
+                        echo -n "$memU% "
+                        VSZ=$(cat /proc/$word/statm | awk '{print $1}')
+                        echo -n "$VSZ "
+                        stat=$(grep State /proc/$word/status | awk '{print $2}')
+                        echo -n "$stat "
+                        Files=$(sudo ls -l /proc/$word/fd | awk '{print $11}' | paste -d, -s)
+                        echo "$Files "
+
+
                 fi
 
         done;
