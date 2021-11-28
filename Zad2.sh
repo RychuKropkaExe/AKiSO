65ddb1a789ef00e4683bf06a035f3a4f3883ff59
RychuKropkaExe <81522252+RychuKropkaExe@users.noreply.github.com>
2021-11-28 22:02:08 +0100

    Update Zad2.sh

--- Zad2.sh
+++ Zad2.sh
@@ -1,13 +1,13 @@
-#!/usr/bin/bash
+#!/user/bin/bash
 ls /proc | grep '[0-9]' > /tmp/Aprc
-echo PID: USER: %CPU %MEM VSZ STAT FILES
+printf "PID\tUSER\t%%CPU\t%%MEM\tVSZ\tSTAT\tFILES\t \n"
 while read -a line; do
         for word in ${line[@]}; do
                 if test -d "/proc/$word"; then
-                        echo -n "$word "
+                        printf "$word\t"
                         userid=$(grep Uid /proc/$word/status | awk '{print $2}')
                         user=$(grep -w "$userid" /etc/passwd | cut -d: -f1)
-                        echo -n  "$user "
+                        printf "$user\t"
                         utime=$(cat /proc/$word/stat | awk '{print $14}')
                         stime=$(cat /proc/$word/stat | awk '{print $15}')
                         starttime=$(cat /proc/$word/stat | awk '{print $21}')
@@ -15,16 +15,16 @@ while read -a line; do
                         time=$(bc -l <<< "scale=0;$utime+$stime")
                         seconds=$(bc -l <<< "scale=0;($uptime-($starttime/100))")
                         cpu_usage=$(bc -l <<< "scale=2;$time/$seconds")
-                        echo -n "$cpu_usage% "
+                        printf "$cpu_usage%%\t"
                         mem=$(cat /proc/$word/statm | awk '{print $2}')
                         memU=$(bc -l <<< "scale=2; (100*($mem/1024))/7829")
-                        echo -n "$memU% "
+                        printf "$memU%%\t"
                         VSZ=$(cat /proc/$word/statm | awk '{print $1}')
-                        echo -n "$VSZ "
+                        printf "$VSZ\t"
                         stat=$(grep State /proc/$word/status | awk '{print $2}')
-                        echo -n "$stat "
-                        Files=$(sudo ls -l /proc/$word/fd | awk '{print $11}' | paste -d, -s)
-                        echo "$Files "
+                        printf "$stat\t"
+                        Files=$(ls -l /proc/$word/fdinfo | wc -l)
+                        printf "$Files\n"
 
 
                 fi
